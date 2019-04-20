77e20252c40cd8c7985a152f02884fb1e7c77af9
Scott Fraser <scottfraserdev@gmail.com>
2019-04-20 10:58:00 -0700

    updated to be built with cli 3

--- src/main.js
+++ src/main.js
@@ -1,21 +1,9 @@
 import Vue from "vue";
-import App from "./App";
-import Vuetify from "vuetify";
-import "vuetify/dist/vuetify.min.css";
-// add this two import statements
-import AOS from "aos";
-import "aos/dist/aos.css";
-
-Vue.use(Vuetify);
+import "./plugins/vuetify";
+import App from "./App.vue";
 
 Vue.config.productionTip = false;
 
-/* eslint-disable no-new */
 new Vue({
-  el: "#app",
-  created() {
-    AOS.init({ disable: "phone" });
-  }, // add this to initialize AOS
-  components: { App },
-  template: "<App/>"
-});
+  render: h => h(App)
+}).$mount("#app");
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,10 @@
+import Vue from "vue";
+import Vuetify from "vuetify/lib";
+import "vuetify/src/stylus/app.styl";
+import "@fortawesome/fontawesome-free/css/all.css";
+
+Vue.use(Vuetify, {
+  icons: {
+    iconfont: "fa"
+  }
+});
